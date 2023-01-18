150d1b311e198576a2edff403fd454643c79ac66
Serena Seyler <18663457+sseyler0119@users.noreply.github.com>
2023-01-18 10:17:40 -0800

    moved Adv JS to new repo

--- Advanced JS/CarsAndDronesApp/src/classes/vehicle.js
+++
@@ -1,9 +0,0 @@
-export class Vehicle
-{
-    // base constructor with fields common to both cars and drones
-    constructor(license, model, latLong) {
-        this.license = license;
-        this.model = model;
-        this.latLong = latLong;
-    }
-}
--- Advanced JS/CarsAndDronesApp/src/framework/page.js
+++
@@ -1,9 +0,0 @@
-import { BaseElement } from "../ui/base-element.js";
-
-export class Page extends BaseElement {
-    
-    constructor(pageTitle) {
-        super();
-        this.pageTitle = pageTitle;
-    }
-}
