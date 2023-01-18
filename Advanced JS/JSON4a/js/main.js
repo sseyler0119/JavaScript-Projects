e53ac5bee13b9ab15f62de24f79bf88f6723d161
Serena Seyler <18663457+sseyler0119@users.noreply.github.com>
2023-01-17 18:55:44 -0800

    parse arrays

--- Advanced JS/JSON4a/js/main.js
+++ Advanced JS/JSON4a/js/main.js
@@ -1,2 +1,36 @@
 // JavaScript Document
 
+var xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'data.json', true);
+xhr.responseType = 'text';
+xhr.send();
+
+xhr.onload = function() {
+    if(xhr.status === 200){
+        var myStuff = JSON.parse(xhr.response);
+        console.log(myStuff);
+        for(i = 0; i < myStuff.presidents.length; i++) {
+            console.log(myStuff.presidents[i].first);
+            console.log(myStuff.presidents[i].last);
+            console.log(myStuff.presidents[i].served);
+
+            console.log(myStuff.vicepresidents[i].first);
+            console.log(myStuff.vicepresidents[i].last);
+        } // end for loop
+        
+        var myString= "";
+        for(i = 0; i < myStuff.presidents.length; i++) {
+            var x= i + 1;
+            myString += "<br>President " + x + " was ";
+            myString += myStuff.presidents[i].first + " ";
+            myString += myStuff.presidents[i].last + ". ";
+
+            myString += "He served from " + myStuff.presidents[i].served + " with ";
+            myString += myStuff.vicepresidents[i].first + " ";
+            myString += myStuff.vicepresidents[i].last + " ";
+        } // end for loop
+        
+        document.getElementById("message").innerHTML = myString;
+    } // end if
+} // end function
