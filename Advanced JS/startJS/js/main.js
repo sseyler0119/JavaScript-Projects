2d214afd3c21e9aaf7857103a9332e6e3bf96992
Serena Seyler <18663457+sseyler0119@users.noreply.github.com>
2023-01-17 18:16:16 -0800

    set up XMLHttp response and parse data

--- Advanced JS/startJS/js/main.js
+++ Advanced JS/startJS/js/main.js
@@ -20,14 +20,26 @@ var myObj2 = JSON.parse(theData2);
 console.log(myObj2);
 document.getElementById("message").innerHTML = myObj2.Jane.degree.BA;
 
-
-
+// parse data from file
 var xhr = new XMLHttpRequest(); 
 // open sets the parameters
 xhr.open('GET', 'data.json', true);
-xhr.send(); // send actually initates the open request
+xhr.responseType = 'text';
 
 xhr.onreadystatechange = function () {
-    console.log(xhr.readyState);
-    console.log(xhr.status);
+    console.log(xhr.readyState); //onload function runs when ready state = 4
+    console.log(xhr.status); // status must be 200 or the file hasn't loaded correctly
+    console.log(xhr.statusText);
 } // end function
+
+// parse the data
+xhr.onload = function () {
+    if(xhr.status === 200) {
+        var myStuff = JSON.parse(xhr.responseText);
+        console.log(myStuff);
+    }
+    
+}// end function
+
+xhr.send(); // send actually initates the open request
+
